42750d5de88f8ef19d0ad7dd6844ab04bd2d36e5
Thomas P Reji <thomaspreji333@gmail.com>
2025-01-21 23:42:28 +0530

    caesar_cipher

--- TUTORIAL 2/1.Caesar_Cipher.py
+++ TUTORIAL 2/1.Caesar_Cipher.py
@@ -1,16 +1,40 @@
-#ENCRYPTION
-text=str(input("Enter a text: "))
-key = int(input("Enter key value: "))
-encrypted_text=""
+print("MENU\n1.ENCRYPTION\n2.DECRYPTION\n3.EXIT")
+choice=int(input("Enter choice: "))
 
-for char in text:
-    if char.isalpha():
-        shift = key%26
-        if char.islower():
-            encrypted_text += chr((ord(char)-ord('a') + shift)%26 + ord('a'))
-        elif char.isupper():
-            encrypted_text += chr((ord(char) - ord('A') + shift)%26 + ord('A'))
-    else:
-        encrypted_text +=char
+if choice == 1: #ENCRYPTION
+    text=str(input("Enter text to encrypt: "))
+    key = int(input("Enter key value: "))
+    encrypted_text=""
 
-print("Encrypted Text : " +encrypted_text)
+    for char in text:
+        if char.isalpha():
+            shift = key%26
+            if char.islower():
+                encrypted_text += chr((ord(char)-ord('a') + shift)%26 + ord('a'))
+            elif char.isupper():
+                encrypted_text += chr((ord(char) - ord('A') + shift)%26 + ord('A'))
+        else:
+            encrypted_text +=char
+    print("Encrypted Text : " +encrypted_text)
+
+elif choice == 2: #DECRYPTION
+    text=str(input("Enter text to decrypt: "))
+    key = int(input("Enter key value: "))
+    decrypted_text=""
+
+    for char in text:
+        if char.isalpha():
+            shift = key%26
+            if char.isupper():
+                decrypted_text += chr((ord(char) - ord('A') - shift)%26 + ord('A')) 
+            elif char.islower():
+                decrypted_text += chr((ord(char) - ord('a') - shift)%26 + ord('a'))
+        else:
+            decrypted_text +=char
+    print("Decrypted Text : " +decrypted_text)
+
+elif choice ==3: #EXIT
+    exit()
+
+else:
+    print("Invalid choice")
