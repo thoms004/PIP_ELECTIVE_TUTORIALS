3dd74d3a4393e40c1c8d64a51dba98e0bd7aa10f
Thomas P Reji <thomaspreji333@gmail.com>
2025-01-23 22:24:46 +0530

    binary_decimal

---
+++ TUTORIAL 2/2.BinarytoDecimal.py
@@ -0,0 +1,8 @@
+binary = input("Enter binary number: ")
+decimal = 0
+power = 0
+
+for digit in binary[::-1]:
+    decimal += int(digit) * (2 ** power)
+    power += 1
+print("Decimal equivalent of " + binary + " is " + str(decimal))
